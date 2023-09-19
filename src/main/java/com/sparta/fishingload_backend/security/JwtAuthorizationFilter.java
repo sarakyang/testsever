2ded3919de5817bf93048b7471ee6898a1f7002e
ghdnjs18 <ghdnjs18@naver.com>
2023-09-19 23:03:48 +0900

    Refresh Token 검증 수정

--- src/main/java/com/sparta/fishingload_backend/security/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/fishingload_backend/security/JwtAuthorizationFilter.java
@@ -1,6 +1,5 @@
 package com.sparta.fishingload_backend.security;
 
-import com.sparta.fishingload_backend.entity.RefreshToken;
 import com.sparta.fishingload_backend.entity.UserRoleEnum;
 import com.sparta.fishingload_backend.repository.RefreshTokenRepository;
 import io.jsonwebtoken.Claims;
@@ -20,47 +19,48 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-    public class JwtAuthorizationFilter extends OncePerRequestFilter {
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
-        private final JwtUtil jwtUtil;
-        private final UserDetailsServiceImpl userDetailsService;
-        private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     // 필터 검증
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String accessToken = jwtUtil.getJwtFromHeader(request, JwtUtil.AUTHORIZATION_HEADER);
-        String refreshToken = jwtUtil.getJwtFromHeader(request, JwtUtil.REFRESH_HEADER);
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = jwtUtil.getJwtFromHeader(req, JwtUtil.AUTHORIZATION_HEADER);
+        String refreshToken = jwtUtil.getJwtFromHeader(req, JwtUtil.REFRESH_HEADER);
 
-        if (request.getRequestURI().equals("/user/login") || request.getRequestURI().equals("/user/signup")) {
-            filterChain.doFilter(request, response);
+        if (req.getRequestURI().equals("/user/login") || req.getRequestURI().equals("/user/signup")) {
+            logger.info("Pass Authorization");
+            filterChain.doFilter(req, res);
         }
 
-        // 토큰이 null인지, 길이가 0인지, 공백이 포함 되어 있는지 확인
+        // 토큰이 null인지, 길이가 0인지, 공백이 포함 되어 있는지 확인 있으면 (false)
         if (StringUtils.hasText(accessToken)) {
 
             if (!jwtUtil.validateToken(accessToken)) {
-                if (!jwtUtil.validateToken(refreshToken)) {
-                    throw new IllegalArgumentException("유효하지 않는 토큰입니다.");
+
+                String refresh = req.getHeader(JwtUtil.REFRESH_HEADER);
+                if (!jwtUtil.validateToken(refreshToken) || !refreshTokenRepository.existsByToken(refresh)) {
+                    logger.error("Refresh Token Error");
+                    res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    return;
                 }
+
+                logger.info("Access Token Recreate");
                 Claims info = jwtUtil.getUserInfoFromToken(refreshToken);
-                String userId = info.getSubject();
+                String username = info.getSubject();
+                UserRoleEnum role = UserRoleEnum.valueOf(String.valueOf(info.get("auth")));
 
-                RefreshToken existRefreshToken = refreshTokenRepository.findByUserId(userId).orElseThrow(() ->
-                        new IllegalArgumentException("유효하지 않는 토큰입니다."));
-                String existRefreshTokenCode = jwtUtil.substringToken(existRefreshToken.getToken());
-                if (!existRefreshTokenCode.equals(refreshToken)) throw new IllegalArgumentException("유효하지 않는 토큰입니다.");
+                accessToken = jwtUtil.createAccessToken(username, role);
+                res.addHeader(JwtUtil.AUTHORIZATION_HEADER, accessToken);
+                accessToken = jwtUtil.substringToken(accessToken);
             }
-            Claims info = jwtUtil.getUserInfoFromToken(refreshToken);
-            String username = info.getSubject();
-            UserRoleEnum role = UserRoleEnum.valueOf(String.valueOf(info.get("auth")));
 
-            accessToken = jwtUtil.createAccessToken(username, role);
-            response.addHeader(JwtUtil.AUTHORIZATION_HEADER, accessToken);
-            accessToken = jwtUtil.substringToken(accessToken);
+            Claims info = jwtUtil.getUserInfoFromToken(accessToken);
 
-            info = jwtUtil.getUserInfoFromToken(accessToken);
-
+            logger.info("Token Authentication");
             try {
                 setAuthentication(info.getSubject());
             } catch (Exception e) {
@@ -68,7 +68,7 @@ import java.io.IOException;
             }
         }
 
-        filterChain.doFilter(request, response);
+        filterChain.doFilter(req, res);
     }
 
     // 권한 인증 처리
