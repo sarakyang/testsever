3ac85a046230b24554671fc89a02b3f5c6408649
sarakyang <co0co2@naver.com>
2023-09-21 17:21:25 +0900

    위치 변경

--- src/test/java/com/sparta/fishingload_backend/FishingLoadBackEndApplicationTests.java
+++ src/test/java/com/sparta/fishingload_backend/FishingLoadBackEndApplicationTests.java
@@ -1,13 +1,10 @@
 package com.sparta.fishingload_backend;
 
+import com.sparta.fishingload_backend.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FishingLoadBackEndApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
